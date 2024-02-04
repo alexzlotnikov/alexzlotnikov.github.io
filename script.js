bf885169522cc64404ae8ebb472d7c291a7a8cd9
Alex Zlotnikov <107267208+alexzlotnikov@users.noreply.github.com>
2024-02-04 19:53:04 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function sendNotification() {
+    // You can use different methods to send notifications, but for simplicity, we'll use an alert.
+    alert('Leezah misses you!');
+
+    // You can add code here to send a notification to your phone using a third-party service.
+    // For example, you can use services like Pushbullet, Pushover, or Firebase Cloud Messaging (FCM).
+}
